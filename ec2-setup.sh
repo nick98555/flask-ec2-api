bcb167e7746726f62e81b06dc843c126e7ec51fb
nick98555 <63329784+nick98555@users.noreply.github.com>
2025-07-12 21:54:49 -0400

    Update ec2-setup.sh
    Fixed git clone URL

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -5,7 +5,7 @@ sudo apt update -y
 sudo apt install -y python3-pip git
 
 # Clone your GitHub repo (update with your actual repo later)
-git clone https://github.com/your-username/flask-ec2-api.git
+git clone https://github.com/nick98555/flask-ec2-api.git
 cd flask-ec2-api
 
 # Install dependencies
