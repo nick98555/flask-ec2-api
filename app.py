098da66c5d378781c1bbfa2d69a2ca735435b3b8
nick98555 <63329784+nick98555@users.noreply.github.com>
2025-07-20 20:19:13 -0400

    updated dashboard landing page to display API stats

--- app.py
+++ app.py
@@ -128,7 +128,9 @@ def api_logs_dashboard():
     conn.close()
     return render_template("apilogs.html", logs=logs)
 
- 
+ @app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
     
 def init_db():
     conn = sqlite3.connect("apilogs.db")
